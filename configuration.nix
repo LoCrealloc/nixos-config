05b7cd58a44807541724b7522aa4b58d56963787
LoC <loc@locrealloc.de>
2024-03-08 08:49:28 +0100

    removed stateversion comment

--- configuration.nix
+++ configuration.nix
@@ -16,6 +16,7 @@ in
   time.timeZone = "Europe/Berlin";
 
   i18n.defaultLocale = "de_DE.UTF-8";
+
   console = {
     useXkbConfig = true;
   };
@@ -25,7 +26,6 @@ in
  
   services.udev = {
     packages = [
-      pkgs.android-udev-rules
       pkgs.platformio-core
       pkgs.openocd
     ];
@@ -60,13 +60,7 @@ in
     pkgs.wineWowPackages.stable
   ];
 
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "22.05"; # Did you read the comment?
+  system.stateVersion = "22.05";
 
 }
 
