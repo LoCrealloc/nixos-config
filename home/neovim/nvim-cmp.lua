d4ddb261b7b6ead4af7c15da6c3a7bff822d2455
LoC <loc@locrealloc.de>
2024-04-16 09:06:56 +0200

    disabled nvim autocompletion on CR

--- home/neovim/nvim-cmp.lua
+++ home/neovim/nvim-cmp.lua
@@ -25,7 +25,7 @@ cmp.setup({
 		['<C-f>'] = cmp.mapping.scroll_docs(4),
 		['<C-Tab>'] = cmp.mapping.complete(),
 		['<C-e>'] = cmp.mapping.abort(),
-		['<CR>'] = cmp.mapping.confirm({ select = true }),
+		-- ['<CR>'] = cmp.mapping.confirm({ select = true }),
 
 		["<Tab>"] = cmp.mapping(function(fallback)
 			if cmp.visible() then
