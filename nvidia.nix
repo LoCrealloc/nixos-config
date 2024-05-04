f5e6023c9cad684c70f141e0f99b15660e91fa5e
LoC <loc@locrealloc.de>
2024-05-04 07:05:08 +0200

    Restructured nvidia configuration and changed power related options

--- nvidia.nix
+++ nvidia.nix
@@ -1,22 +1,36 @@
 { lib, ... }: {
   hardware.opengl.enable = true;
-  hardware.nvidia.modesetting.enable = true;
 
-  hardware.nvidia.prime = {
-    offload.enable = true;
+  hardware.nvidia = {
 
-    nvidiaBusId = "PCI:1:00:0";
+    modesetting.enable = true;
+    powerManagement = {
+      enable = true;
+      finegrained = true;
+    };
 
-    amdgpuBusId = "PCI:6:00:0";
+    prime = {
+      offload.enable = true;
+
+      nvidiaBusId = "PCI:1:00:0";
+
+      amdgpuBusId = "PCI:6:00:0";
+    };
   };
 
   specialisation = {
     external-display.configuration = {
       system.nixos.tags = [ "external-display" ];
-      hardware.nvidia.prime.offload.enable = lib.mkForce false;
-      hardware.nvidia.powerManagement.enable = lib.mkForce false;
-
-      hardware.nvidia.prime.sync.enable = lib.mkForce true;
+      hardware.nvidia = {
+        prime = {
+          offload.enable = lib.mkForce false;
+          sync.enable = lib.mkForce true;
+        };
+        powerManagement = {
+          enable = lib.mkForce false;
+          finegrained = lib.mkForce false;
+        };
+      };
     };
   };
 }
