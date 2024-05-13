0d6abd6254b13dfa6de7f9e807398440a0811bd8
LoC <loc@locrealloc.de>
2024-05-13 13:31:18 +0200

    changed zsh formatting

--- home/zsh.nix
+++ home/zsh.nix
@@ -1,27 +1,30 @@
-{
-  programs.zsh = {
-    enable = true;
-
-    oh-my-zsh = {
+{ pkgs, ... }: {
+  programs.zsh =
+    {
       enable = true;
-      # theme = "bureau";
-      theme = "juanghurtado";
-      plugins = [ "git" "ssh-agent" "sudo" "dotenv" ];
-      extraConfig = ''
-        zstyle :omz:plugins:ssh-agent identities id_ed25519
+
+      oh-my-zsh = {
+        enable = true;
+        # theme = "bureau";
+        theme = "juanghurtado";
+        plugins = [ "git" "ssh-agent" "sudo" "dotenv" ];
+        extraConfig = ''
+          zstyle :omz:plugins:ssh-agent identities id_ed25519
+        '';
+      };
+      shellAliases = {
+        cpshot = "cp ~/Pictures/screenshots/$(ls -t ~/Pictures/screenshots | head -n1)";
+        nixdev = "nix develop --command zsh";
+      };
+      sessionVariables = {
+        EDITOR = "nvim";
+      };
+      initExtra = ''
+        export QT_SCREEN_SCALE_FACTORS=1.25
+        export GPG_TTY=$(tty)
+        gpgconf --launch gpg-agent
+        function gi() { curl -sLw n https://www.toptal.com/developers/gitignore/api/$@ ;}
       '';
+
     };
-    shellAliases = {
-      cpshot = "cp ~/Pictures/screenshots/$(ls -t ~/Pictures/screenshots | head -n1)";
-    };
-    sessionVariables = {
-      EDITOR = "nvim";
-    };
-    initExtra = ''
-      export QT_SCREEN_SCALE_FACTORS=1.25
-      export GPG_TTY=$(tty)
-      gpgconf --launch gpg-agent
-      function gi() { curl -sLw n https://www.toptal.com/developers/gitignore/api/$@ ;}
-    '';
-  };
 }
