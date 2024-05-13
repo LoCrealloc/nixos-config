f45431bc1214788381a9101944a4d779ae324cdf
LoC <loc@locrealloc.de>
2024-05-13 19:04:56 +0200

    removed opaciry changes from picom

--- home/picom.nix
+++ home/picom.nix
@@ -4,22 +4,6 @@
     backend = "xrender";
 
     shadow = false;
-
-    activeOpacity = 1;
-    inactiveOpacity = 0.95;
-    menuOpacity = 0.98;
-    opacityRules = [
-      "99:name *= 'YouTube'"
-      "99:name *= 'Picture-in-Picture'"
-      "99:class_g = 'vlc'"
-      "95:class_g = 'URxvt' && !_NET_WM_STATE@:32a"
-      "0:_NET_WM_STATE@[0]:32a *= '_NET_WM_STATE_HIDDEN'"
-      "0:_NET_WM_STATE@[1]:32a *= '_NET_WM_STATE_HIDDEN'"
-      "0:_NET_WM_STATE@[2]:32a *= '_NET_WM_STATE_HIDDEN'"
-      "0:_NET_WM_STATE@[3]:32a *= '_NET_WM_STATE_HIDDEN'"
-      "0:_NET_WM_STATE@[4]:32a *= '_NET_WM_STATE_HIDDEN'"
-    ];
-
     settings = {
       corner-radius = 2;
     };
