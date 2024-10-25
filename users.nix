af2fa7f960614914e84dabdfc03453e3e7d1d1aa
LoC <loc@locrealloc.de>
2024-10-25 11:19:15 +0200

    added user to wireshark group + reformatting

--- users.nix
+++ users.nix
@@ -1,4 +1,5 @@
-{ pkgs, config, ... }: {
+{ pkgs, config, ... }:
+{
   users.groups = {
     borg = {
       name = "borg";
@@ -12,7 +13,19 @@
     isNormalUser = true;
     home = "/home/loc";
     shell = pkgs.zsh;
-    extraGroups = [ "wheel" "networkmanager" "docker" "video" "camera" "borg" "keyboard" "libvirtd" "dialout" "plugdev" ];
+    extraGroups = [
+      "wheel"
+      "networkmanager"
+      "docker"
+      "video"
+      "camera"
+      "borg"
+      "keyboard"
+      "libvirtd"
+      "dialout"
+      "plugdev"
+      "wireshark"
+    ];
     hashedPasswordFile = config.sops.secrets."user/hashedPassword".path;
   };
 
@@ -20,7 +33,9 @@
     isNormalUser = true;
     home = "/srv/copy";
     homeMode = "777";
-    openssh.authorizedKeys.keys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIA0vi1RQ03861k7jdykz9OpWImFnoAZN+4qHJg0/ufOE wolfsblut@wolfswald" ];
+    openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIA0vi1RQ03861k7jdykz9OpWImFnoAZN+4qHJg0/ufOE wolfsblut@wolfswald"
+    ];
   };
 
   home-manager.users = {
