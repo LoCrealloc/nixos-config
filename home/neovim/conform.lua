22775f7e46357c592bcb98a40957b44e33773df5
LoC <loc@locrealloc.de>
2025-02-17 13:06:41 +0100

    Adjusted conform setup to new format

--- home/neovim/conform.lua
+++ home/neovim/conform.lua
@@ -1,9 +1,9 @@
 require("conform").setup({
 	formatters_by_ft = {
 		python = { "isort", "black" },
-		javascript = { { "prettierd", "prettier" } },
-		markdown = { { "prettierd", "prettier" } },
-		nix = { { "nixfmt", "alejandra" } },
+		javascript = { "prettierd" },
+		markdown = { "prettierd" },
+		nix = { "nixfmt" },
 		tex = { "latexindent" },
 		rust = { "rustfmt" },
 	},
