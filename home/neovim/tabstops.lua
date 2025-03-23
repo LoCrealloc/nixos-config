e4749d20e12cedcd0edc0f06320c929d8833c777
LoC <loc@locrealloc.de>
2025-03-23 13:22:11 +0100

    added custom tabstops for hugo _index.md files

--- home/neovim/tabstops.lua
+++ home/neovim/tabstops.lua
@@ -20,3 +20,27 @@ vim.api.nvim_create_autocmd(
 		end
 	}
 )
+
+
+-- Hugo Markdown
+vim.api.nvim_create_autocmd(
+	{"BufRead", "BufNewFile", "BufEnter"},
+	{
+		pattern={"_index.md"}, 
+		callback=function() 
+			vim.api.nvim_set_option_value("expandtab", true, {})
+			vim.api.nvim_set_option_value("tabstop", 2, {})
+		end
+	}
+)
+
+vim.api.nvim_create_autocmd(
+	"BufLeave",
+	{
+		pattern={"_index.md"}, 
+		callback=function() 
+			vim.api.nvim_set_option_value("expandtab", false, {})
+			vim.api.nvim_set_option_value("tabstop", 4, {})
+		end
+	}
+)
