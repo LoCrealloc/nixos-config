f6d867341996d9cb4776a2093bc7289260dd0c83
LoCrealloc <loc@locrealloc.de>
2025-05-01 19:42:59 +0200

    switch to amdvlk drivers

--- hosts/desktop/modules/graphics.nix
+++ hosts/desktop/modules/graphics.nix
@@ -6,6 +6,7 @@
 
   hardware.graphics = {
     enable = true;
+    extraPackages = [ pkgs.amdvlk ];
   };
 
   services.xserver.videoDrivers = [
---
+++ hosts/desktop/modules/wol.nix
@@ -0,0 +1,6 @@
+{
+  networking.interfaces."enp14s0".wakeOnLan = {
+    enable = true;
+  };
+  # TODO SSH server for password entry
+}
