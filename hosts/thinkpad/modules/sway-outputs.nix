4a6a4ad566b8a230e56845f5060eebeb816933f9
LoCrealloc <loc@locrealloc.de>
2025-07-15 09:05:47 +0200

    added wallpaper for thinkpad

--- hosts/thinkpad/modules/sway-outputs.nix
+++ hosts/thinkpad/modules/sway-outputs.nix
@@ -1,8 +1,8 @@
 {
   home-manager.users.loc.wayland.windowManager.sway.config.output = {
-    #"DP-3" = {
-    #  background = "${../../../wallpapers/explosion_purple.jpeg} fit";
-    #  position = "0 0";
-    #};
+    "eDP-1" = {
+      background = "${../../../wallpapers/explosion_purple.jpeg} fit";
+      position = "0 0";
+    };
   };
 }
