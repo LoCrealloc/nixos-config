5242297773afe91e2045b8eceb6a0bd8fe425516
LoCrealloc <loc@locrealloc.de>
2025-11-15 11:03:40 +0100

    enhanced flameshot service

--- home/wayland/flameshot.nix
+++ home/wayland/flameshot.nix
@@ -1,11 +1,22 @@
+{ config, pkgs, ... }:
+let
+  screenshotsDir = "${config.home.homeDirectory}/Pictures/screenshots";
+in
 {
+  systemd.user.tmpfiles.rules = [
+    "d ${screenshotsDir} 0740 loc users -"
+  ];
+
   services.flameshot = {
     enable = true;
+    package = pkgs.flameshot.override { enableWlrSupport = true; };
     settings = {
       General = {
-        savePath = "~/Pictures/screenshots";
+        savePath = screenshotsDir;
         saveAsFileExtension = ".png";
         useJpgForClipboard = false;
+        showDesktopNotification = false;
+        filenamePattern = "screenshot_%Y%m%d_%H%M%S";
       };
     };
   };
