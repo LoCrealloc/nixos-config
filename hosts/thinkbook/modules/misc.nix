fab0d31351ddd89674cb92bcccf5f9441bf81d3d
LoC <loc@locrealloc.de>
2025-12-04 14:50:09 +0100

    removed deprecated logind settings

--- hosts/thinkbook/modules/misc.nix
+++ hosts/thinkbook/modules/misc.nix
@@ -1,11 +1,4 @@
 {
-  services.logind = {
-    lidSwitch = "suspend-then-hibernate";
-    lidSwitchDocked = "suspend-then-hibernate";
-    extraConfig = ''
-      HandlePowerKey=hibernate
-    '';
-  };
   systemd.sleep.extraConfig = ''
     HibernateDelaySec=60m
   '';
