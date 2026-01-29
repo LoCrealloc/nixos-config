61c53e7b08aa4c9885a5ddff1e60998d9d7c2d38
LoC <loc@locrealloc.de>
2026-01-29 14:44:16 +0100

    fixed YET ANOTHER obsidian.nvim breaking change

--- home/neovim/obsidian.lua
+++ home/neovim/obsidian.lua
@@ -163,7 +163,6 @@ require("obsidian").setup({
     post_setup = function(client) end,
 
     -- Runs anytime you enter the buffer for a note.
-    ---@param client obsidian.Client
     ---@param note obsidian.Note
     enter_note = function(note)
 		if os.execute("[ \"$(pgrep -fl obsidian | grep electron | wc -l)\" = '0' ]") ~= 0 and vim.fn.filereadable(tostring(note.path)) ~= 0 then
@@ -201,21 +200,8 @@ require("obsidian").setup({
 
   -- Specify how to handle attachments.
   attachments = {
-    -- The default folder to place images in via `:ObsidianPasteImg`.
-    -- If this is a relative path it will be interpreted as relative to the vault root.
-    -- You can always override this per image by passing a full path to the command instead of just a filename.
-    img_folder = "assets/images",  -- This is the default
-
-    -- A function that determines the text to insert in the note when pasting an image.
-    -- It takes two arguments, the `obsidian.Client` and an `obsidian.Path` to the image file.
-    -- This is the default implementation.
-    ---@param client obsidian.Client
-    ---@param path obsidian.Path the absolute path to the image file
-    ---@return string
-    img_text_func = function(client, path)
-      path = client:vault_relative_path(path) or path
-      return string.format("![%s](%s)", path.name, path)
-    end,
+    confirm_img_paste = false,
+    img_folder = "assets/images",
   },
 })
 
